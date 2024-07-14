5150aed8e4e96660763e0dcc8714230889098149
johjan <johjanmartinez.15@gmail.com>
2024-07-14 11:07:12 -0500

    se crea microservice

---
+++ src/main/java/com/task/live/entity/Task.java
@@ -0,0 +1,29 @@
+package com.task.live.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table (name = "t_task")
+public class Task {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long  id;
+    private String title;
+    private String description;
+    private String status;
+
+}
